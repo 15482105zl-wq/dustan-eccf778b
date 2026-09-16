5439ae423c2bd2297dafefd7f0f527a9db78b882
15482105zl-wq <15482105zl@gmail.com>
2026-09-16 20:31:44 +0800

    更新 Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,7 @@ const resourceLinks = [
   { icon: Globe, label: "夸克网盘", color: "text-primary", url: "https://pan.quark.cn/s/29e2744b31f1" },
   { icon: Shield, label: "UC网盘", color: "text-primary", url: "https://drive.uc.cn/s/b2243269454c4" },
 ];
-const APP_DOWNLOAD_URL = "https://pan.quark.cn/s/4b40771599e9";
+const APP_DOWNLOAD_URL = "https://pan.quark.cn/s/d9edf950d8fd";
 const Index = () => {
   const navigate = useNavigate();
   const [shared, setShared] = useState(false);
