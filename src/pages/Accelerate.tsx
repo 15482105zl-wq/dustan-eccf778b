0fbc8754f7b747b642f087a0f7289a3c7b9d66e1
15482105zl-wq <15482105zl@gmail.com>
2026-09-18 21:56:52 +0800

    更新 Accelerate.tsx

--- src/pages/Accelerate.tsx
+++ src/pages/Accelerate.tsx
@@ -8,7 +8,7 @@ import UserNav from "@/components/UserNav";
 
 const NICE_URL = "https://dustan.mmmoyou.com/#/register?code=0lc8ncSH";
 const LVCHA_URL = "https://pan.quark.cn/s/1d9113e678f3";
-const APP_DOWNLOAD_URL = "https://pxyfbbohoazbslneagix.supabase.co/storage/v1/object/public/downloads//Dustan Hub.apk";
+const APP_DOWNLOAD_URL = "https://pxyfbbohoazbslneagix.supabase.co/storage/v1/object/public/downloads//DustanHub.apk";
 const Accelerate = () => {
   const navigate = useNavigate();
   const [couponCopied, setCouponCopied] = useState(false);
