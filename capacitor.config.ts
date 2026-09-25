bedaf05b95c79cf817426d5fa2b229411df06995
Dustan <15482105zl@gmail.com>
2026-09-25 20:12:13 +0800

    添加闪屏Logo配置。

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -10,6 +10,14 @@ const config: CapacitorConfig = {
   },
   android: {
     allowMixedContent: false
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      backgroundColor: '#000000',
+      androidSplashResourceName: 'splash',
+      showSpinner: false
+    }
   }
 };
 
